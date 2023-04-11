5f6274489c28a96d7708adbb1e059ba47d69c4fe
John Wormald <johnwormald@outlook.com>
2023-04-11 18:18:57 +0100

    Upgrade to .Net 7.0

---
+++ UserRepositoryService.cs
@@ -0,0 +1,55 @@
+﻿namespace MinimalWebAPi.DataAccess
+{
+    public record UserDto(int Id, string Name, string UserName, string Password, string Role);
+
+    public record User(int Id, string Name, [Required] string UserName, [Required] string Password, string Role);
+
+    public interface IUserRepositoryService
+    {
+        Task LoadUsers(List<User> users);
+        Task<UserDto> GetUser(User userModel);
+        Task<UserDto> GetUserByUserName(string userName);
+    }
+    public class UserRepositoryService : IUserRepositoryService
+    {
+        private readonly MinimalApiDb _db;
+
+        public UserRepositoryService(MinimalApiDb db)
+        {
+            _db = db;
+        }
+        public async Task LoadUsers(List<User> users)
+        {
+            await _db.Users.AddRangeAsync(users);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<UserDto> GetUser(User userModel)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(x => string.Equals(x.UserName, userModel.UserName) && string.Equals(x.Password, userModel.Password));
+            if (user == null)
+                return null;
+
+            var userDto = new UserDto(user.Id, user.Name, user.UserName, user.Password, user.Role);
+            return userDto;
+        }
+
+        public async Task<UserDto> GetUserByUserName(string userName)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(x => string.Equals(x.UserName, userName));
+            if (user == null)
+                return null;
+
+            var userDto = new UserDto(user.Id, user.Name, user.UserName, user.Password, user.Role);
+            return userDto;
+        }
+    }
+
+    public class MinimalApiDb : DbContext
+    {
+        public MinimalApiDb(DbContextOptions<MinimalApiDb> options)
+            : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ UserService.cs
@@ -0,0 +1,66 @@
+﻿using MinimalWebAPi.DataAccess;
+using static System.Net.WebRequestMethods;
+
+namespace MinimalWebAPi
+{
+    public interface IUserService
+    {
+        Task LoadUsers();
+        Task<UserDto> GetUser(User userModel);
+        Task<UserDto> GetUserByUserName(string userName);
+        Task<string> UserJwt(User userModel);
+    }
+    public class UserService : IUserService
+    {
+        private IUserRepositoryService _repository;
+        private ITokenService _tokenService;
+        private IConfiguration _config;
+
+        public UserService(IUserRepositoryService repository, ITokenService tokenService, IConfiguration config)
+        {
+            _repository = repository;
+            _tokenService = tokenService;
+            _config = config;
+        }
+        public async Task LoadUsers()
+        {
+            List<User> users = new List<User>
+            {
+                new(1, "John Wormald", "johnw", "P@55w0rd!", "Boss"),
+                new(2, "Thomas", "tom", "P@55w0rd!", "Manager"),
+                new(3, "Richard", "dick", "P@55w0rd!", "Developer"),
+                new(4, "Harry", "harry", "P@55w0rd!", "User")
+            };
+
+            await _repository.LoadUsers(users);
+        }
+
+        public async Task<UserDto> GetUser(User userModel)
+        {
+            return await _repository.GetUser(userModel);
+        }
+
+        public async Task<UserDto> GetUserByUserName(string userName)
+        {
+            return await _repository.GetUserByUserName(userName);
+
+        }
+
+        public async Task<string> UserJwt(User userModel)
+        {
+            if (userModel == null)
+                return string.Empty;
+
+            var userDto = await this.GetUser(userModel);
+
+            var token = _tokenService.BuildToken(
+                _config["Jwt:Key"] ?? string.Empty,
+                _config["Jwt:Issuer"] ?? string.Empty,
+                _config["Jwt:Audience"] ?? string.Empty,
+                userDto);
+
+           return token;
+        }
+
+    }
+}
